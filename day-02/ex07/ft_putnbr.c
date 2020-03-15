3b865c720dc40d89bdff9f2254762545e33d6119
Marina <marina.sarrias@gmail.com>
2020-03-15 23:52:50 +0100

    added integer to string

--- day-02/ex07/ft_putnbr.c
+++ day-02/ex07/ft_putnbr.c
@@ -9,22 +9,49 @@
 /*   Updated: 2020/03/12 19:21:33 by hmarina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
- void	ft_putnbr(int nb){
-	 char number;
-	 char peel_number;
+int    reverse_digits(int num)
+{
+    int rev_num;
 
-	 if (nb <= 9)
-	 {
-		 number = 48 + nb % 10;
-		 write(1, &number, 1);
-	 } 
-	 else
-	 {
-		 number = 48 + nb % 10;
-		 write(1, &number, 1);
+    rev_num= 0;
+    while(num > 0)
+    {
+        rev_num = rev_num * 10 + num % 10;
+        num = num / 10;
+    }
+    return rev_num;
+}
 
-		 peel_number = nb / 10;
-		 ft_putnbr(peel_number);
-	 }
+void    ft_writenbr(int nb)
+{
+    char number;
+
+    number = 48 + nb % 10;
+    write(1, &number, 1);
+}
+
+void    ft_putnbr(int nb)
+{
+   char number;
+   char peel_number;
+
+    if (nb < 0)
+    {
+        write(1, "-", 1);
+        nb = - nb;
+    }
+    if (nb <= 9)
+    {
+        ft_writenbr(nb);
+    }
+    else
+        {
+        nb = reverse_digits(nb);
+        ft_writenbr(nb);
+        peel_number = nb / 10;
+        ft_putnbr(peel_number);
+        }
+}
